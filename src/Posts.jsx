00b147bc50dddfe0e51a76d275f835101d24fea9
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-05-06 12:51:48 +0600

    Used state for store data not for load data and used useEffect.

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Posts() {
 
     const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        
+    }, [])
 
     return(
         <div>
